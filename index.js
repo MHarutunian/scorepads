70614243e9e10043e50300ab9dc0bcfd74219981
LAPTOP-I96RF18Q\marco <marco.wilms@live.de>
2017-10-07 17:18:33 +0200

    Added useless stuff from VSC.

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
- 
+
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.get('/scorepads', (req, res) => {
+  res.send('These are insane score pads!');
+});
+
+app.get('/api/players', (req, res) => {
+  res.send('Jan, Krista, Linus, Lennart, Matteo, Marco');
 });
  
 app.listen(80);
