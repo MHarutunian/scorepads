913aa30c8c23f98f53ac6b6e9d8de80e07669ce5
LAPTOP-I96RF18Q\marco <marco.wilms@live.de>
2017-10-09 16:38:03 +0200

    schreibTabelle erstellt, 4 Spieler können ausgewählt werden und werden beim klick von "spiel beginnen" automatisch in die Tabelle übertragen

--- web/js/doppelkopf.js
+++ web/js/doppelkopf.js
@@ -5,6 +5,9 @@ function addPlayers(select, players) {
     option.appendChild(document.createTextNode(player.name));
     select.appendChild(option);
   }
+  for (let i = 1; i < 5; i += 1) { // Vorauswahl auf empty setzen
+    document.getElementById(`player${i}`).selectedIndex = -1;
+  }
 }
 
 window.onload = function onLoad() {
@@ -12,12 +15,32 @@ window.onload = function onLoad() {
 
   request.open('GET', '/api/players');
   request.addEventListener('load', function onRequestLoad() {
-    if (request.status >= 200 && request.status < 300) {
-      const players = JSON.parse(request.responseText);
-      addPlayers(document.getElementById('player'), players);
-    } else {
-      console.warn(request.statusText, request.responseText);
+    for (let i = 1; i < 5; i += 1) { // 4 Spieler können ausgewählt werden
+      if (request.status >= 200 && request.status < 300) {
+        const players = JSON.parse(request.responseText);
+        addPlayers(document.getElementById(`player${i}`), players);
+      } else {
+        console.warn(request.statusText, request.responseText);
+      }
     }
   });
   request.send();
 };
+
+document.addEventListener('DOMContentLoaded', function storeChoice() { // Auswahl des Spielers wird gespeichert
+  for (let i = 1; i < 5; i += 1) {
+    const input = document.getElementById(`player${i}`);
+    if (localStorage[`player${i}`]) {
+      input.value = localStorage[`player${i}`];
+    }
+    input.onchange = function storeChange() {
+      localStorage[`player${i}`] = this.value;
+    };
+  }
+});
+
+function writeNames() { // Schreibe Spieler in Tabelle(innerHTML, wegen Mangel an alternativ Wissen)
+  for (let i = 1; i < 5; i += 1) {
+    document.getElementById(`spieler${i}`).innerHTML = localStorage[`player${i}`];
+  }
+}
