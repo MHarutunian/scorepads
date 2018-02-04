2640920bbbcfb32fbddeeffba2ff34c784eeb1db
Matteo Harutunian <matteo.harutunian@netlight.com>
2018-02-04 17:21:41 +0100

    Added CSS for match popup.

--- server/index.js
+++ server/index.js
@@ -3,9 +3,10 @@ const bodyParser = require('body-parser');
 const players = require('./api/players');
 const scorepads = require('./api/scorepads');
 const config = require('./config/config');
+const domain = require('domain');
 
 const app = express();
-
+const appDomain = domain.create();
 
 if (config.dev) {
   /* eslint-disable global-require,import/no-extraneous-dependencies */
@@ -23,6 +24,11 @@ if (config.dev) {
 
   app.use(webpackHotMiddleware(compiler));
 }
+
+// error handler for uncaught exceptions, prevents app from crashing in case of errors
+appDomain.on('error', (error) => {
+  console.log(error); // eslint-disable-line no-console
+});
 
 // we increase the body limit here to allow bigger images in profile picture uploads
 // in the future we probably want to scale the images before uploading them
